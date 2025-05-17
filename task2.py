0bdc5dd13ba4a4d8ac78d8ec63569cc53a9f9fb7
ralinavalieva <r04161767@gmail.com>
2025-05-17 14:38:35 +0300

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,14 @@
+ import numpy as np
+import matplotlib.pyplot as plt
+
+# Генерация случайных данных
+data = np.random.normal(loc=0, scale=1, size=1000)
+
+# Построение гистограммы
+plt.figure(figsize=(8, 5))
+plt.hist(data, bins=20, edgecolor='black')
+plt.xlabel('Значение')
+plt.ylabel('Частота')
+plt.title('Гистограмма случайных данных (нормальное распределение)')
+plt.grid(True)
+plt.show()
